d87b7a1753114d256cd95c8082659f0d2af26e45
BALAJI V <156071515+Drago1723K@users.noreply.github.com>
2026-02-05 17:52:44 +0530

    PHMS Source Code.ino

--- PHMS Source Code.ino
+++ PHMS Source Code.ino
@@ -1,86 +1,230 @@
-
+//MAX30100 ESP32 WebServer
+#include <WiFi.h>
+#include <WebServer.h>
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
-#define BLYNK_PRINT Serial
-#include <Blynk.h>
-#include <WiFi.h>
-#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include "DHT.h"
-#define DHTTYPE DHT11
-#define REPORTING_PERIOD_MS 1000
 #define DS18B20 5
-
-char auth[] = "G9nCH4W7AWUwviplzwpGLleH0fOdXF9_";
-char ssid[] = "PHMS";
-char pass[] = "123456789";
-PulseOximeter pox;
-
-uint8_t DHTPin = 18;
-DHT dht(DHTPin, DHTTYPE);
-float Temperature;
-float Humidity;
-float bodytemperature;
-float BPM, SpO2;
-uint32_t tsLastReport = 0;
-
 OneWire oneWire(DS18B20);
 DallasTemperature sensors(&oneWire);
+DHT dht(4,DHT11);
+#define relay1 2
 
-void onBeatDetected() {
-  Serial.println("Beat Detected!");
-}
+#define REPORTING_PERIOD_MS     1500
+float tempC;
+float tempF; // Declare as global variable
+float humidity; // Declare as global variable
+float BPM, SpO2;
+float F;
+
+/*Put your SSID & Password*/
+const char* ssid = "OnePlus Nord CE 3 5G";  // Enter SSID here
+const char* password = "63645577";  //Enter Password here
+
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+
+WebServer server(80);
+
+void onBeatDetected()
+{
+  Serial.println("Beat Detected!");
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(19, OUTPUT);
-  pinMode(DHTPin, INPUT);
-  dht.begin();
-  Blynk.begin(auth, ssid, pass);
-  Serial.print("Initializing Pulse Oximeter..");
+  Serial.begin(115200);
+  dht.begin();
+  sensors.begin();
+  pinMode(19, OUTPUT);
+  pinMode(relay1, OUTPUT); 
+  delay(100);
+  Serial.println("Connecting to ");
+  Serial.println(ssid);
 
-  if (!pox.begin()) {
-    Serial.println("FAILED");
-    for (;;)
-      ;
-  } else {
-    Serial.println("SUCCESS");
-    pox.setOnBeatDetectedCallback(onBeatDetected);
-  }
-  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+  //connect to your local wi-fi network
+  WiFi.begin(ssid, password);
+
+//check wi-fi is connected to wi-fi network
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  
+  Serial.println(WiFi.localIP());
+  
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+    if (!pox.begin()) {
+    for (;;);
+  } else {
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+  }
+
+  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+}
+void loop() {
+  server.handleClient();
+  sensors.requestTemperatures();
+  tempC = sensors.getTempCByIndex(0);
+  F=(tempC*9/5)+32;
+  tempF = dht.readTemperature(true);
+  humidity = dht.readHumidity();
+  if (tempC < 37) {    // Check if temperature is less than 36 degrees
+    digitalWrite(relay1, HIGH);  // Turn on relay
+} else {
+    digitalWrite(relay1, LOW); // Turn off relay
+}
+pox.update();
+BPM = pox.getHeartRate();
+SpO2 = pox.getSpO2();
+
+if (millis() - tsLastReport > REPORTING_PERIOD_MS)
+  {
+    Serial.print("Body Temperature : ");
+    Serial.print(F);
+    Serial.println(" *F");
+
+    Serial.print("Room Temperature : ");
+    Serial.print(tempF);
+    Serial.println(" *F");
+
+    Serial.print("Humidity : ");
+    Serial.print(humidity);
+    Serial.println(" %");
+
+    Serial.print("BPM: ");
+    Serial.println(BPM);
+
+    Serial.print("SpO2: ");
+    Serial.print(SpO2);
+    Serial.println("%");
+
+    Serial.println("*********************************");
+    Serial.println();
+
+    tsLastReport = millis();
 }
 
-void loop() {
-  pox.update();
-  Blynk.run();
-  Temperature = dht.readTemperature(); 
-  Humidity = dht.readHumidity();       
-  BPM = pox.getHeartRate();
-  SpO2 = pox.getSpO2();
-  bodytemperature = sensors.getTempCByIndex(0);
+}
 
-  if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
-    Serial.print("Heart rate:");
-    Serial.print(BPM);
-    Serial.print(" bpm / SpO2:");
-    Serial.print(SpO2);
-    Serial.println(" %");
-    Serial.print("Room Temperature: ");
-    Serial.print(Temperature);
-    Serial.println("°C");
-    Serial.print("Room Humidity: ");
-    Serial.print(Humidity);
-    Serial.println("%");
-    Serial.print("Body Temperature: ");
-    Serial.print(bodytemperature);
-    Serial.println("°C");
-    Blynk.virtualWrite(V3, Temperature);
-    Blynk.virtualWrite(V4, Humidity);
-    Blynk.virtualWrite(V5, BPM);
-    Blynk.virtualWrite(V6, SpO2);
-    Blynk.virtualWrite(V7, bodytemperature);
+void handle_OnConnect() {
+  server.send(200, "text/html", SendHTML(BPM, SpO2, tempF, F, humidity));
+}
 
-    tsLastReport = millis();
-  }
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(float BPM, float SpO2, float tempF, float F, float humidity) {
+  String ptr = "<!DOCTYPE html>";
+  ptr += "<html>";
+  ptr += "<head>";
+  ptr += "<title>Pulse Oximeter ESP32 WebServer</title>";
+  ptr += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+  ptr += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css'>";
+  ptr += "<link rel='stylesheet' type='text/css' href='styles.css'>";
+  ptr += "<style>";
+  ptr += "body { background-color: #fff; font-family: sans-serif; color: #333333; font: 14px Helvetica, Bree-serif box-sizing: border-box;}";
+  ptr += "#page { margin: 20px; background-color: #fff;}";
+  ptr += ".container { height: inherit; padding-bottom: 20px;}";
+  ptr += ".header { padding: 20px;}";
+  ptr += ".header h1 { padding-bottom: 0.3em; color: #008080; font-size: 45px; font-weight: bold; font-family: Garmond, 'sans-serif'; text-align: center;}";
+  ptr += "h2 { padding-bottom: 0.2em; border-bottom: 1px solid #eee; margin: 2px; text-align: left;}";
+  ptr += ".header h3 { font-weight: bold; font-family: Arial, 'sans-serif'; font-size: 17px; color: #b6b6b6; text-align: center;}";
+  ptr += ".box-full { padding: 20px; border 1px solid #ddd; border-radius: 1em 1em 1em 1em; box-shadow: 1px 7px 7px 1px rgba(0,0,0,0.4); background: #fff; margin: 20px; width: 300px;}";
+  ptr += "@media (max-width: 494px) { #page { width: inherit; margin: 5px auto; } #content { padding: 1px;} .box-full { margin: 8px 8px 12px 8px; padding: 10px; width: inherit;; float: none; } }";
+  ptr += "@media (min-width: 494px) and (max-width: 980px) { #page { width: 465px; margin 0 auto; } .box-full { width: 380px; } }";
+  ptr += "@media (min-width: 980px) { #page { width: 930px; margin: auto; } }";
+  ptr += ".sensor { margin: 12px 0px; font-size: 2.5rem;}";
+  ptr += ".sensor-labels { font-size: 1rem; vertical-align: middle; padding-bottom: 15px;}";
+  ptr += ".units { font-size: 1.2rem;}";
+  ptr += "hr { height: 1px; color: #eee; background-color: #eee; border: none;}";
+  ptr += "</style>";
+
+
+
+  //Ajax Code Start
+  ptr += "<script>\n";
+  ptr += "setInterval(loadDoc,1500);\n";
+  ptr += "function loadDoc() {\n";
+  ptr += "var xhttp = new XMLHttpRequest();\n";
+  ptr += "xhttp.onreadystatechange = function() {\n";
+  ptr += "if (this.readyState == 4 && this.status == 200) {\n";
+  ptr += "document.body.innerHTML =this.responseText}\n";
+  ptr += "};\n";
+  ptr += "xhttp.open(\"GET\", \"/\", true);\n";
+  ptr += "xhttp.send();\n";
+  ptr += "}\n";
+  ptr += "</script>\n";
+  //Ajax Code END
+
+  ptr += "</head>";
+  ptr += "<body>";
+  ptr += "<div id='page'>";
+  ptr += "<div class='header'>";
+  ptr += "<h1>Patient Health Monitoring System ESP32 WebServer</h1>";
+  ptr += "<h3>BY TEAM 10</h3>";
+  ptr += "</div>";
+  ptr += "<div id='content' align='center'>";
+  ptr += "<div class='box-full' align='left'>";
+  ptr += "<h2>Sensor Readings</h2>";
+  ptr += "<div class='sensors-container'>";
+
+  //For Heart Rate
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-heartbeat' style='color:#cc3300'></i>";
+  ptr += "<span class='sensor-labels'> Heart Rate </span>";
+  ptr += (int)BPM;
+  ptr += "<sup class='units'>BPM</sup>";
+  ptr += "</p>";
+  ptr += "<hr>";
+
+  //For Sp02
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-burn' style='color:#f7347a'></i>";
+  ptr += "<span class='sensor-labels'> Sp02 </span>";
+  ptr += (float)SpO2;
+  ptr += "<sup class='units'>%</sup>";
+  ptr += "</p>";
+  ptr += "<hr>";
+
+ // For Room Temperature (in Fahrenheit)
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-thermometer-half' style='color:#0099cc'></i>";
+  ptr += "<span class='sensor-labels'> Room Temperature (Fahrenheit) </span>";
+  ptr += (float)tempF;
+  ptr += "<sup class='units'>&deg;F</sup>";
+  ptr += "</p>";
+  ptr += "<hr>";
+
+  // For Body Temperature (in Celsius)
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-thermometer-half' style='color:#0099cc'></i>";
+  ptr += "<span class='sensor-labels'> Body Temperature (Fahrenheit) </span>";
+  ptr += (float)F;
+  ptr += "<sup class='units'>&deg;F</sup>";
+  ptr += "</p>";
+  ptr += "<hr>";
+
+  // For Humidity
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-tint' style='color:#0099cc'></i>";
+  ptr += "<span class='sensor-labels'> Humidity </span>";
+  ptr += (float)humidity;
+  ptr += "<sup class='units'>%</sup>";
+  ptr += "</p>";
+
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</body>";
+  ptr += "</html>";
+  return ptr;
 }
